386cfc4478cebb4f81ef3778af6bccb22a1a37ad
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-06-06 05:52:39 -0400

    added easy pid controller

--- Field-Oriented-Differential-Drive-Example/src/main/java/frc/robot/Utils/MechanismControllers/MechanismController.java
+++ Field-Oriented-Differential-Drive-Example/src/main/java/frc/robot/Utils/MechanismControllers/MechanismController.java
@@ -1,5 +1,6 @@
 package frc.robot.Utils.MechanismControllers;
 
 public interface MechanismController {
+    /** gets the calculate correction power that should be applied to the mechanism */
     double getMotorPower(double mechanismVelocity, double mechanismPosition);
 }
