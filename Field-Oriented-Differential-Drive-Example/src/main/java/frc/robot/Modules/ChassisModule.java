3ebe67f5b1df6f659110b33c80ea1dcbb0ff862e
capz1 <163543505+capz1@users.noreply.github.com>
2024-06-11 12:48:10 +0800

    added field centric dev code

--- Field-Oriented-Differential-Drive-Example/src/main/java/frc/robot/Modules/ChassisModule.java
+++ Field-Oriented-Differential-Drive-Example/src/main/java/frc/robot/Modules/ChassisModule.java
@@ -1,9 +1,15 @@
 package frc.robot.Modules;
 
 import frc.robot.Drivers.Motors.Motor;
+import frc.robot.Utils.MathUtils.Vector2D;
+import frc.robot.Utils.MechanismControllers.EasyPIDController;
 import frc.robot.Utils.RobotModuleOperatorMarker;
-
 public class ChassisModule extends RobotModuleBase {
+    public enum DriveMode {
+        BASIC,
+        FIELD_CENTRIC
+    }
+    private DriveMode driveMode;
     final Motor left, right;
     // positive is forward, positive is counter-clockwise
     private double forward, turn;
@@ -15,6 +21,44 @@ public class ChassisModule extends RobotModuleBase {
 
     @Override
     public void init() {
+    }
+
+    private final EasyPIDController controller = new EasyPIDController(
+            new EasyPIDController.EasyPIDProfile(
+                    0.7, // limit the chassis motor power to 70%
+                    Math.toRadians(60), // the chassis starts slowing down when the error is smaller than 90 degrees
+                    0.05, // to move the chassis, it takes at least 5% the motor power
+                    Math.toRadians(2), // if the error is within 2 degrees, we ignore it
+                    Math.toRadians(5), // if the error is wihtin 5 degrees, we think the chassis is in position
+                    0.3, // the chassis needs 0.3 seconds to slow down
+                    true // when the robot turns 360 degrees, it goes back to 0 degrees.  we call this "in cycle"
+            ), 0 // the chassis starts at zero degrees
+    );
+
+    @Override
+    protected void periodic(double dt) {
+        /* called 50 times every second */
+
+        switch (driveMode) {
+            case BASIC -> {
+                /* set power to left and right motor */
+                left.setPower(forward - turn * 0.5, this);
+                right.setPower(forward + turn * 0.5, this);
+            }
+            case FIELD_CENTRIC -> {
+                if (motion.getMagnitude() < 0.1) {
+                    left.setPower(0, this);
+                    right.setPower(0, this);
+                    return;
+                }
+                motion.getMagnitude();
+                motion.getHeading();
+            }
+        }
+    }
+
+    @Override
+    public void onReset() {
         /* called once when robot started */
 
         /* gain ownership to left and right motors */
@@ -24,20 +68,9 @@ public class ChassisModule extends RobotModuleBase {
         /* prevent random movements */
         left.setPower(0 ,this);
         right.setPower(0, this);
-    }
 
-    @Override
-    protected void periodic(double dt) {
-        /* called 50 times every second */
-
-        /* set power to left and right motor */
-        left.setPower(forward - turn * 0.5, this);
-        right.setPower(forward + turn * 0.5, this);
-    }
-
-    @Override
-    public void onReset() {
-
+        this.driveMode = DriveMode.FIELD_CENTRIC;
+        this.motion = new Vector2D();
     }
 
     /**
@@ -63,4 +96,13 @@ public class ChassisModule extends RobotModuleBase {
 
         this.turn = turn;
     }
+
+    private Vector2D motion;
+    public void setMotion(Vector2D motion, RobotModuleOperatorMarker operator){
+        if (!isOwner(operator))
+            return;
+
+        this.motion = motion;
+    }
 }
+
